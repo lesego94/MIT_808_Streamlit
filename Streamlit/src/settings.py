f3723ec38d3077a71e9f7a67d1875b7f0a3e7c61
Lesego Matojane <lesegomat@nedbankinsurance.co.za>
2023-05-24 17:10:09 +0200

    created docker

--- Streamlit/src/settings.py
+++ Streamlit/src/settings.py
@@ -15,45 +15,40 @@ import os
 # # Get the relative path of the root directory with respect to the current working directory
 # ROOT = root_path.relative_to(Path.cwd())
 
-ROOT  = current_dir = os.getcwd() 
-# ROOT = os.path.dirname(current_dir)
+# current_dir = os.getcwd() 
 
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Source
 IMAGE = 'Image'
 VIDEO = 'Video'
-# RTSP = 'RTSP'
-# YOUTUBE = 'YouTube'
 
 SOURCES_LIST = [IMAGE, VIDEO]
 
 # images
-IMAGES_DIR = f"{ROOT}/lib/Examples/images"
-DEFAULT_IMAGE = f'{IMAGES_DIR}/example_2.png'
-DEFAULT_DETECT_IMAGE = f'{IMAGES_DIR}/croc_detected.jpeg'
+IMAGES_DIR = os.path.join(ROOT, 'lib', 'Examples', 'images')
+DEFAULT_IMAGE = os.path.join(IMAGES_DIR, 'example_2.png')
+DEFAULT_DETECT_IMAGE = os.path.join(IMAGES_DIR, 'croc_detected.jpeg')
 
-#video
-VIDEO_DIR = f'{ROOT}/lib/Examples/videos'
-VIDEO_1_PATH = f'{VIDEO_DIR}/DJI_0411.mp4'
+# video
+VIDEO_DIR = os.path.join(ROOT, 'lib', 'Examples', 'videos')
+VIDEO_1_PATH = os.path.join(VIDEO_DIR, 'DJI_0411.mp4')
 
 VIDEOS_DICT = {
     'DJI_0411': VIDEO_1_PATH
 }
 
 # model
-MODEL_DIR = f'{ROOT}/lib/models/weights'
-DETECTION_MODEL = f'{MODEL_DIR}/yolov8n.pt'
-SEGMENTATION_MODEL = f'{MODEL_DIR}/best.pt'
-
+MODEL_DIR = os.path.join(ROOT, 'lib', 'models', 'weights')
+DETECTION_MODEL = os.path.join(MODEL_DIR, 'yolov8n.pt')
+SEGMENTATION_MODEL = os.path.join(MODEL_DIR, 'best.pt')
 
 # Detected/segmented image dirpath locator
 DETECT_LOCATOR = 'detect'
 SEGMENT_LOCATOR = 'segment'
 
 
-# Webcam
-WEBCAM_PATH = 0
-\
+
 
 
 import streamlit as st
