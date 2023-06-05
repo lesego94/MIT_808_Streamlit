3c0a8fdef47d8fc483841c2f842c8ab79ab678b0
Lesego Matojane <lmatojane@gmail.com>
2023-06-05 19:27:20 +0200

    removed detect button

--- Streamlit/src/helper.py
+++ Streamlit/src/helper.py
@@ -1,7 +1,7 @@
 import datetime
 
 from ultralytics import YOLO
-
+from PIL import Image, ExifTags
 
 def load_model(model_path):
     model = YOLO(model_path)
@@ -42,3 +42,22 @@ def get_Date():
     formatted_time = gmt_plus_2_time.strftime("%Y-%m-%d")
     
     return str(formatted_time)
+
+
+def get_coordinates(img): 
+    try:    
+        exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
+        
+        if 'GPSInfo' not in exif:
+            return "None", "None"
+        
+        if 1 not in exif['GPSInfo'] or 2 not in exif['GPSInfo'] or 3 not in exif['GPSInfo'] or 4 not in exif['GPSInfo']:
+            return "Incomplete GPSInfo in EXIF data", "Incomplete GPSInfo in EXIF data"
+        
+        
+        Lat = f"{exif['GPSInfo'][1]}: {', '.join(map(str, exif['GPSInfo'][2]))}"
+        Long= f"{exif['GPSInfo'][3]}: {', '.join(map(str, exif['GPSInfo'][4]))}"
+
+        return Lat, Long
+    except AttributeError:
+        return "No EXIF data", "No EXIF data"
